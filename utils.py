9377b6ec3337f45262614b18d20752c0d96f3813
Yong-Hwan Yoo <ajoiej729@gmail.com>
2020-07-30 14:16:50 +0900

    make train_test_split a function

--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
 import os
 import random
 
+def train_test_split():
+    files = os.listdir("convdata")
+    length = len(files)
+    list_txt = []
+    train_txt = []
+    test_txt = []
 
-files = os.listdir("convdata")
-length = len(files)
-list_txt = []
-train_txt = []
-test_txt = []
-
-for file in files:
-    # print(file.split('.')[0]+'_v0')
-    data = file.split('.')[0]+'_v0'
-    list_txt.append(data)
-    if random.random() > 0.7:
-        test_txt.append(data)
-    else:
-        train_txt.append(data)
-    data = file.split('.')[0]+'_v1'
-    list_txt.append(data)
-    if random.random() > 0.7:
-        test_txt.append(data)
-    else:
-        train_txt.append(data)
+    for file in files:
+        # print(file.split('.')[0]+'_v0')
+        data = file.split('.')[0]+'_v0'
+        list_txt.append(data)
+        if random.random() > 0.7:
+            test_txt.append(data)
+        else:
+            train_txt.append(data)
+        data = file.split('.')[0]+'_v1'
+        list_txt.append(data)
+        if random.random() > 0.7:
+            test_txt.append(data)
+        else:
+            train_txt.append(data)
