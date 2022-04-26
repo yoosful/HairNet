f8c590530b72edb85c99ce0fd8be1384761c8312
manhdung20112000 <manhdung20112000@gmail.com>
2022-04-26 16:26:50 +0700

    fix model

--- src/dataloader.py
+++ src/dataloader.py
@@ -13,67 +13,61 @@ from torch.utils.data import Dataset, DataLoader
 from .preprocessing import gen_RT_matrix, get_rendered_convdata, gen_vis_weight, gasuss_noise
 
 class HairNetDataset(Dataset):
-    def __init__(self, data_path, train_flag=1, noise_flag=1):
-        '''
-        param data_path: the path of project, such as '/home/albertren/Workspace/HairNet/HairNet-ren'
+    def __init__(self, project_dir, train_flag=1, noise_flag=1):
+        """
+        param project_dir: the path of project, such as '/home/albertren/Workspace/HairNet/HairNet-ren'
         param train_flag: train_flag=1 -> generate training dataset, train_flag=0 -> generate testing dataset
-        '''
-        self.data_path = data_path
+        """
+        self.project_dir = project_dir
         self.train_flag = train_flag
         self.noise_flag = noise_flag
         self.toTensor = transforms.ToTensor()
+        train_path = self.project_dir + "/data/index/train.txt"
+        test_path = self.project_dir + "/data/index/test.txt"
+        self.data_path = self.project_dir + "/data/"
+        self.convdata_path = self.project_dir + "/convdata/"
         # generate dataset
         if self.train_flag == 1:
             self.train_index = []
-            self.train_index_path = self.data_path + '/index/train.txt'
-            with open(self.train_index_path, 'r') as f:
+            self.train_index_path = train_path
+            with open(self.train_index_path, "r") as f:
                 lines = f.readlines()
                 for x in lines:
-                    self.train_index.append(x.strip().split(' '))
+                    self.train_index.append(x.strip().split(" "))
         if self.train_flag == 0:
             self.test_index = []
-            self.test_index_path = self.data_path + '/index/test.txt'
-            with open(self.test_index_path, 'r') as f:
+            self.test_index_path = test_path
+            with open(self.test_index_path, "r") as f:
                 lines = f.readlines()
                 for x in lines:
-                    self.test_index.append(x.strip().split(' '))
+                    self.test_index.append(x.strip().split(" "))
 
     def __getitem__(self, index):
         if self.train_flag == 1:
             current_index = self.train_index[index]
-            current_convdata_index = re.search('strands\d\d\d\d\d_\d\d\d\d\d_\d\d\d\d\d', str(current_index)).group(0)
-            current_RT_mat = gen_RT_matrix(self.data_path+str(current_index[0])+'.txt')
-            current_convdata_path = self.data_path+'/convdata/'+str(current_convdata_index)+'.convdata'
-            current_convdata = get_rendered_convdata(current_convdata_path, current_RT_mat)
-            current_visweight = gen_vis_weight(self.data_path+str(current_index[0])+'.vismap')
-            if self.noise_flag == 1:
-                current_img = cv2.imread(self.data_path+str(current_index[0])+'.png')
-                current_img = gasuss_noise(current_img)
-                current_img = self.toTensor(current_img)
-            else:
-                current_img = cv2.imread(self.data_path+str(current_index[0])+'.png')
-                current_img = self.toTensor(current_img)
         else:
             current_index = self.test_index[index]
-            current_convdata_index = re.search('strands\d\d\d\d\d_\d\d\d\d\d_\d\d\d\d\d', str(current_index)).group(0)
-            current_RT_mat = gen_RT_matrix(self.data_path+str(current_index[0])+'.txt')
-            current_convdata_path = self.data_path+'/convdata/'+str(current_convdata_index)+'.convdata'
-            current_convdata = get_rendered_convdata(current_convdata_path, current_RT_mat)
 
-            current_visweight = gen_vis_weight(self.data_path+str(current_index[0])+'.vismap')
-            if self.noise_flag == 1:
-                current_img = cv2.imread(self.data_path+str(current_index[0])+'.png')
-                current_img = gasuss_noise(current_img)     
-                current_img = self.toTensor(current_img)
-            else:
-                current_img = cv2.imread(self.data_path+str(current_index[0])+'.png')
-                current_img = self.toTensor(current_img)
+        current_convdata_index = re.search(
+            "strands\d\d\d\d\d_\d\d\d\d\d_\d\d\d\d\d", str(current_index)
+        ).group(0)
+        current_convdata_path = (
+            self.convdata_path + str(current_convdata_index) + ".convdata"
+        )
+        current_convdata = np.load(current_convdata_path).reshape(100, 4, 32, 32)
+        current_visweight = gen_vis_weight(
+            self.data_path + str(current_index[0]) + ".vismap"
+        )
+        current_img = cv2.imread(self.data_path + str(current_index[0]) + ".png")
+
+        if self.noise_flag == 1:
+            current_img = gasuss_noise(current_img)
+        current_img = self.toTensor(current_img)
+
         return current_img, current_convdata, current_visweight
-    
+
     def __len__(self):
         if self.train_flag == 1:
             return len(self.train_index)
         else:
-            return len(self.test_index)    
-
-
+            return len(self.test_index)
--- src/model.py
+++ src/model.py
@@ -16,91 +16,192 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # encoder
-        self.conv1 = nn.Conv2d(3, 32, 3, 2, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 2, 1)
-        self.conv3 = nn.Conv2d(64, 128, 3, 2, 1)
-        self.conv4 = nn.Conv2d(128, 256, 3, 2, 1)
-        self.conv5 = nn.Conv2d(256, 512, 3, 2, 1)
+        # self.conv1 = nn.Conv2d(3, 32, 8, 2, 3)
+        self.conv2 = nn.Conv2d(3, 64, 8, 2, 3)
+        self.conv3 = nn.Conv2d(64, 128, 6, 2, 2)
+        self.conv4 = nn.Conv2d(128, 256, 4, 2, 1)
+        self.conv5 = nn.Conv2d(256, 512, 4, 2, 1)
         # decoder
-        self.fc1 = nn.Linear(1*1*512, 4*4*256)
+        self.fc1 = nn.Linear(512, 4096)
+        # self.fc2 = nn.Linear(1024, 4096)
         self.conv6 = nn.Conv2d(256, 512, 3, 1, 1)
         self.conv7 = nn.Conv2d(512, 512, 3, 1, 1)
         self.conv8 = nn.Conv2d(512, 512, 3, 1, 1)
         # MLP
         # Position
-        self.branch1_fc1 = nn.Linear(512*32*32, 32)
-        self.branch1_fc2 = nn.Linear(32, 32)
-        self.branch1_fc3 = nn.Linear(32, 32*32*300)
+        self.branch1_fc1 = nn.Conv2d(512, 512, 1, 1, 0)
+        self.branch1_fc2 = nn.Conv2d(512, 512, 1, 1, 0)
+        self.branch1_fc3 = nn.Conv2d(512, 300, 1, 1, 0)
         # Curvature
-        self.branch2_fc1 = nn.Linear(512*32*32, 32)
-        self.branch2_fc2 = nn.Linear(32, 32)
-        self.branch2_fc3 = nn.Linear(32, 32*32*100)
-        
-    def forward(self, x):
+        self.branch2_fc1 = nn.Conv2d(512, 512, 1, 1, 0)
+        self.branch2_fc2 = nn.Conv2d(512, 512, 1, 1, 0)
+        self.branch2_fc3 = nn.Conv2d(512, 100, 1, 1, 0)
+
+    def forward(self, x, interp_factor=1):
         # encoder
-        x = F.relu(self.conv1(x)) # (batch_size, 32, 128, 128)
-        x = F.relu(self.conv2(x)) # (batch_size, 64, 64, 64)
-        x = F.relu(self.conv3(x)) # (batch_size, 128, 32, 32)
-        x = F.relu(self.conv4(x)) # (batch_size, 256, 16, 16)
-        x = F.relu(self.conv5(x)) # (batch_size, 512, 8, 8)
-        x = F.max_pool2d(x, 8) # (batch_size, 512, 1, 1)
+        # x = F.relu(self.conv1(x)) # (batch_size, 32, 128, 128)
+        x = F.relu(self.conv2(x))  # (batch_size, 64, 64, 64)
+        x = F.relu(self.conv3(x))  # (batch_size, 128, 32, 32)
+        x = F.relu(self.conv4(x))  # (batch_size, 256, 16, 16)
+        x = F.relu(self.conv5(x))  # (batch_size, 512, 8, 8)
+        x = torch.tanh(F.max_pool2d(x, 8))  # (batch_size, 512, 1, 1)
         # decoder
-        x = x.view(-1, 1*1*512)
+        x = x.view(-1, 1 * 1 * 512)
         x = F.relu(self.fc1(x))
+        # x = x.view(-1, 1*1*1024)
+        # x = F.relu(self.fc2(x))
         x = x.view(-1, 256, 4, 4)
-        x = F.relu(self.conv6(x)) # (batch_size, 512, 4, 4)
-        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners = False) # (batch_size, 512, 8, 8)
-        x = F.relu(self.conv7(x)) # (batch_size, 512, 8, 8)
-        x = F.interpolate(x, scale_factor=2, mode='bilinear',align_corners = False) # (batch_size, 512, 16, 16)
-        x = F.relu(self.conv8(x)) # (batch_size, 512, 16, 16)
-        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners = False) # (batch_size, 512, 32, 32)
-        x = x.view(-1, 512*32*32)
+        x = F.relu(self.conv6(x))  # (batch_size, 256, 4, 4)
+        x = F.interpolate(
+            x, scale_factor=2, mode="bilinear", align_corners=False
+        )  # (batch_size, 256, 8, 8)
+        x = F.relu(self.conv7(x))  # (batch_size, 256, 8, 8)
+        x = F.interpolate(
+            x, scale_factor=2, mode="bilinear", align_corners=False
+        )  # (batch_size, 256, 16, 16)
+        x = F.relu(self.conv8(x))  # (batch_size, 256, 16, 16)
+        x = F.interpolate(
+            x, scale_factor=2, mode="bilinear", align_corners=False
+        )  # (batch_size, 256, 32, 32)
+        # interpolate feature map
+        if interp_factor != 1:
+            x = F.interpolate(
+                x, scale_factor=interp_factor, mode="bilinear", align_corners=False
+            )  # (batch_size, 256, 32, 32)
         # MLP
         # Position
         branch1_x = F.relu(self.branch1_fc1(x))
         branch1_x = F.relu(self.branch1_fc2(branch1_x))
-        branch1_x = F.relu(self.branch1_fc3(branch1_x))
-        branch1_x = branch1_x.view(-1, 100, 3, 32, 32)
+        branch1_x = self.branch1_fc3(branch1_x)
+        branch1_x = branch1_x.view(-1, 100, 3, 32 * interp_factor, 32 * interp_factor)
         # Curvature
         branch2_x = F.relu(self.branch2_fc1(x))
         branch2_x = F.relu(self.branch2_fc2(branch2_x))
-        branch2_x = F.relu(self.branch2_fc3(branch2_x))
-        branch2_x = branch2_x.view(-1, 100, 1, 32, 32)
-        x = [branch1_x, branch2_x]
-        return torch.cat(x, 2) # (batch_size, 100, 4, 32, 32)
+        branch2_x = self.branch2_fc3(branch2_x)
+        branch2_x = branch2_x.view(-1, 100, 1, 32 * interp_factor, 32 * interp_factor)
+        x = torch.cat([branch1_x, branch2_x], 2)
+        return x  # (batch_size, 100, 4, 32, 32)
 
 
 class MyLoss(nn.Module):
     def __init__(self):
         super(MyLoss, self).__init__()
+
     def forward(self, output, convdata, visweight):
-        pos_loss = 0.0
-        cur_loss = 0.0
-        for i in range(0,32):
-            for j in range(0,32):
-                pos_loss += (visweight[:,:,i,j].reshape(1,-1).mm(torch.pow((convdata[:,:,0:3,i,j]-output[:,:,0:3,i,j]),2).reshape(-1, 3))).sum()
-                cur_loss += (visweight[:,:,i,j].reshape(1,-1).mm(torch.pow((convdata[:,:,3,i,j]-output[:,:,3,i,j]),2).reshape(-1, 1))).sum()
-        # print(pos_loss/1024.0, cur_loss/1024.0)       
-        return pos_loss/1024.0 + cur_loss/1024.0
+        # removing nested for-loops (0.238449s -> 0.001860s)
+        pos_loss = (
+            visweight[:, :, :, :]
+            .reshape(1, -1)
+            .mm(
+                torch.pow(
+                    (convdata[:, :, 0:3, :, :] - output[:, :, 0:3, :, :]), 2
+                ).reshape(-1, 3)
+            )
+            .sum()
+        )
+        cur_loss = (
+            visweight[:, :, :, :]
+            .reshape(1, -1)
+            .mm(
+                torch.pow((convdata[:, :, 3, :, :] - output[:, :, 3, :, :]), 2).reshape(
+                    -1, 1
+                )
+            )
+            .sum()
+        )
+        col_loss = CollisionLoss().forward(output, convdata)
+        # print(pos_loss/(convdata.shape[0]*convdata.shape[1]*1024.0), cur_loss/(convdata.shape[0]*convdata.shape[1]*1024.0), col_loss)
+        return (
+            pos_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)
+            + 1 * cur_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)
+            + 1e-4 * col_loss
+        )
 
 
-class MyPosEvaluation(nn.Module):
+class CollisionLoss(nn.Module):
     def __init__(self):
-        super(MyPosEvaluation, self).__init__()
+        super(CollisionLoss, self).__init__()
+
     def forward(self, output, convdata):
-        loss = 0.0
-        for i in range(0,32):
-            for j in range(0,32):
-                loss += torch.mean(torch.abs(convdata[:,:,0:3,i,j]-output[:,:,0:3,i,j]))
-        return loss/1024.0
+        x, y, z = 0.005, 1.75, 0.01
+        a, b, c = 0.08, 0.12, 0.1
+        L1 = torch.add(
+            torch.add(
+                torch.abs(output[:, :, 0, :, 1:] - output[:, :, 0, :, :-1]),
+                torch.abs(output[:, :, 1, :, 1:] - output[:, :, 1, :, :-1]),
+            ),
+            torch.abs(output[:, :, 2, :, 1:] - output[:, :, 2, :, :-1]),
+        )
+        D = 1 - torch.add(
+            torch.add(
+                torch.pow((output[:, :, 0, :, 1:] - x) / a, 2),
+                torch.pow((output[:, :, 1, :, 1:] - y) / b, 2),
+            ),
+            torch.pow((output[:, :, 2, :, 1:] - z) / c, 2),
+        )
+        D[D < 0] = 0
+        C = torch.sum(L1 * D)
+        loss = C / (convdata.shape[0] * convdata.shape[1] * 1024.0)
+        return loss
 
 
-class MyCurEvaluation(nn.Module):
+class PosMSE(nn.Module):
     def __init__(self):
-        super(MyCurEvaluation, self).__init__()
-    def forward(self, output, convdata):
-        loss = 0.0
-        for i in range(0,32):
-            for j in range(0,32):
-                loss += torch.mean(torch.abs(convdata[:,:,3,i,j]-output[:,:,3,i,j]))
-        return loss/1024.0
+        super(PosMSE, self).__init__()
+
+    def forward(self, output, convdata, visweight, verbose=False):
+        visweight = visweight[:, :, :, :].reshape(1, -1)
+        e_squared = torch.pow(
+            (convdata[:, :, 0:3, :, :] - output[:, :, 0:3, :, :]), 2
+        ).reshape(-1, 3)
+        loss = visweight.mm(e_squared).sum()
+
+        if verbose:
+            # print("[Position Loss]")
+
+            visweight1 = (visweight == 10).float() * 10
+            vis_loss = visweight1.mm(e_squared).sum()
+            # print(
+            #     "\tvis: ",
+            #     (vis_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)).item(),
+            # )
+
+            visweight2 = (visweight == 0.1).float() * 0.1
+            inv_loss = visweight2.mm(e_squared).sum()
+            # print(
+            #     "\tinv: ",
+            #     (inv_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)).item(),
+            # )
+
+        return loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)
+
+
+class CurMSE(nn.Module):
+    def __init__(self):
+        super(CurMSE, self).__init__()
+
+    def forward(self, output, convdata, visweight, verbose=False):
+        visweight = visweight[:, :, :, :].reshape(1, -1)
+        e_squared = torch.pow(
+            (convdata[:, :, 3, :, :] - output[:, :, 3, :, :]), 2
+        ).reshape(-1, 1)
+        loss = visweight.mm(e_squared).sum()
+
+        if verbose:
+            print("[Curvature Loss]")
+
+            visweight1 = (visweight == 10).float() * 10
+            vis_loss = visweight1.mm(e_squared).sum()
+            # print(
+            #     "\tvis: ",
+            #     (vis_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)).item(),
+            # )
+
+            visweight2 = (visweight == 0.1).float() * 0.1
+            inv_loss = visweight2.mm(e_squared).sum()
+            # print(
+            #     "\tinv: ",
+            #     (inv_loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)).item(),
+            # )
+
+        return loss / (convdata.shape[0] * convdata.shape[1] * 1024.0)
